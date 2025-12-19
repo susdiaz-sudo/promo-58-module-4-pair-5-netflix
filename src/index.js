98ae60a2a5e710124e1068394a2d511ea57779ac
Sus.diaz <sus.diazse@gmail.com>
2025-12-19 11:04:39 +0100

    Fix the host

--- src/index.js
+++ src/index.js
@@ -67,13 +67,16 @@ require("dotenv").config();
 
 // Configurar la conexión a la base de datos MySQL
 const connection = async () => {
+
+
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   });
+  
   return connection;
 };
 
